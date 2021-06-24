b2730fd3e82b5032e947e5cc9d33816721e7c081
jhj0224 <jhj8664@gmail.com>
2021-06-24 22:07:46 +0900

    게시물 작성 완료

--- src/com/sbs/exam/app/App.java
+++ src/com/sbs/exam/app/App.java
@@ -1,5 +1,7 @@
 package com.sbs.exam.app;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.sbs.exam.app.dto.Article;
@@ -9,6 +11,10 @@ public class App {
 		System.out.println("== 텍스트 게시판 시작 ==");
 		
 		Scanner sc = new java.util.Scanner(System.in);
+		
+		List<Article> articles = new ArrayList<>();
+		
+		int articlesLastId = 0;
 		
 		while (true) {
 			System.out.printf("명령어) ");
@@ -22,14 +28,18 @@ public class App {
 				String body = sc.nextLine().trim();
 				
 				Article article = new Article();
-				article.id = 1;
+				article.id = articlesLastId + 1;
 				article.regDate = "2021-06-24 12:12:12";
 				article.updateDate = "2021-06-24 12:12:12";
 				article.title = title;
 				article.body = body;
-				System.out.println(article);
-								
-				break;
+				
+				articles.add(article);
+				
+				articlesLastId++;
+				
+				System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
+				
 			}
 			
 			if (command.equals("/usr/system/exit")) {
