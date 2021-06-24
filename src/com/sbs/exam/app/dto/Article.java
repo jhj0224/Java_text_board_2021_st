1c6c95076d2245f2a11651cc4441f53904c249a5
jhj0224 <jhj8664@gmail.com>
2021-06-24 21:59:18 +0900

    Article 클래스 도입

---
+++ src/com/sbs/exam/app/dto/Article.java
@@ -0,0 +1,16 @@
+package com.sbs.exam.app.dto;
+
+public class Article {
+	public int id;
+	public String regDate;
+	public String updateDate;
+	public String title;
+	public String body;
+	
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
+				+ ", body=" + body + "]";
+	}
+		
+}
