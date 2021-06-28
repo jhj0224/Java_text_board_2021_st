c47622d887512fee9a4973bb896d15d84584c430
jhj0224 <jhj8664@gmail.com>
2021-06-29 00:45:03 +0900

    NeedLogin,NeedLogout 인터셉터 도입

--- src/com/sbs/exam/app/App.java
+++ src/com/sbs/exam/app/App.java
@@ -1,10 +1,15 @@
 package com.sbs.exam.app;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.sbs.exam.app.container.Container;
 import com.sbs.exam.app.controller.Controller;
 import com.sbs.exam.app.dto.Member;
+import com.sbs.exam.app.interceptor.Interceptor;
+import com.sbs.exam.app.interceptor.NeedLoginInterceptor;
+import com.sbs.exam.app.interceptor.NeedLogoutInterceptor;
 
 public class App {
 	Scanner sc;
@@ -36,6 +41,10 @@ public class App {
 				System.out.printf("명령어가 올바르지 않습니다.\n");
 				continue;
 			}
+			
+			if (runInterceptors(rq) == false) {
+				continue;
+			}
 
 			Controller controller = getControllerByRequestUri(rq);
 
@@ -47,6 +56,24 @@ public class App {
 		}
 
 		System.out.println("== 텍스트 게시판 끝 ==");
+	}
+
+	/**
+	 * @param rq
+	 * @return
+	 */
+	private boolean runInterceptors(Rq rq) {				
+		List<Interceptor> interceptors = new ArrayList<>();
+		
+		interceptors.add(Container.getNeedLoginInterceptor());
+		interceptors.add(Container.getNeedLogoutInterceptor());
+		
+		for (Interceptor interceptor  : interceptors) {
+			if ( interceptor.run(rq) == false ) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	private Controller getControllerByRequestUri(Rq rq) {
